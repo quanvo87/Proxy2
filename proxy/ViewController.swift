290022d7bbb920cc464386d339f982f27dae921c
Quan <qvo1987@gmail.com>
2016-08-14 13:25:23 -0500

    Initial Commit

---
+++ proxy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  proxy
+//
+//  Created by Quan Vo on 8/14/16.
+//  Copyright © 2016 Quan Vo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
