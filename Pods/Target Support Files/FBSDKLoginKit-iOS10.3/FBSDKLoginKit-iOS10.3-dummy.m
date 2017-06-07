3c372d58f1e3907bccc0001dbcff5e526e570c60
Quan Vo <quan.vo@ibm.com>
2017-06-07 02:20:54 -0500

    add gitignore

---
+++ Pods/Target Support Files/FBSDKLoginKit-iOS10.3/FBSDKLoginKit-iOS10.3-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FBSDKLoginKit_iOS10_3 : NSObject
+@end
+@implementation PodsDummy_FBSDKLoginKit_iOS10_3
+@end
---
+++ Pods/Target Support Files/FacebookLogin-iOS10.0/FacebookLogin-iOS10.0-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FacebookLogin_iOS10_0 : NSObject
+@end
+@implementation PodsDummy_FacebookLogin_iOS10_0
+@end
