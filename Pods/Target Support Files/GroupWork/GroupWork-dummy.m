1c97960b07aacb0ca3de77edb640aa9deeafcf81
Quan Vo <qvo1987@gmail.com>
2018-01-25 21:43:42 -0600

    dont check in pods

--- Pods/Target Support Files/GroupWork/GroupWork-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GroupWork : NSObject
-@end
-@implementation PodsDummy_GroupWork
-@end
--- Pods/Target Support Files/Pods-ProxyTests/Pods-ProxyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ProxyTests : NSObject
-@end
-@implementation PodsDummy_Pods_ProxyTests
-@end
