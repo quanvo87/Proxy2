e64abb9e46c6fadafdff2a9aa74d987f7ae275f5
Quan Vo <qvo1987@gmail.com>
2018-02-01 18:32:15 -0600

    proxiesobserving

---
+++ Proxy/Controller/LoadingViewController.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class LoadingViewController: UIViewController {
+    private lazy var activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(activityIndicator)
+
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            ])
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [weak self] in
+            self?.activityIndicator.startAnimating()
+        }
+    }
+}
--- Proxy/Model Controller/ProxiesObserving.swift
+++ Proxy/Model Controller/ProxiesObserving.swift
@@ -2,18 +2,18 @@ import FirebaseDatabase
 import FirebaseHelper
 
 protocol ProxiesObserving: ReferenceObserving {
-    func observe(proxiesOwnerId: String, proxiesManager: ProxiesManaging)
+    func observe(proxiesOwnerId: String, callback: @escaping ([Proxy]) -> Void)
 }
 
 class ProxiesObserver: ProxiesObserving {
     private (set) var handle: DatabaseHandle?
     private (set) var ref: DatabaseReference?
 
-    func observe(proxiesOwnerId: String, proxiesManager: ProxiesManaging) {
+    func observe(proxiesOwnerId: String, callback: @escaping ([Proxy]) -> Void) {
         stopObserving()
         ref = try? FirebaseHelper.main.makeReference(Child.proxies, proxiesOwnerId)
-        handle = ref?.queryOrdered(byChild: Child.timestamp).observe(.value) { [weak proxiesManager] data in
-            proxiesManager?.proxies = data.toProxiesArray.reversed()
+        handle = ref?.queryOrdered(byChild: Child.timestamp).observe(.value) { data in
+            callback(data.toProxiesArray.reversed())
         }
     }
 
